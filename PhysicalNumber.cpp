ff7cc3dfea81b87fea38ec4cc21be0ed2d340e95
David <voihanskidavid@gmail.com>
2019-04-14 00:22:12 +0300

    started implementation, can now convert a physical number type to the right one to do arithmetics

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -5,51 +5,98 @@ PhysicalNumber::PhysicalNumber(double num, ariel::Unit unit){
     this->value = num;
     this->unit = unit;
 }
-
-void PhysicalNumber::operator++(){
-
+//getters and setters
+Unit PhysicalNumber::getUnit() const{
+    return this->unit;
 }
-
-void PhysicalNumber::operator--(){
-
+double PhysicalNumber::getValue() const{
+    return this->value;
 }
-const PhysicalNumber PhysicalNumber::operator+=(const PhysicalNumber &other) const{
+void PhysicalNumber::setValue(double x){
+    this->value = x;
+}
+void PhysicalNumber::setUnit(Unit u){
+    this->unit = u;
+}
+//prefix
+const PhysicalNumber& PhysicalNumber::operator++(){
+    double temp = this->getValue();
+    temp++;
+    this->setValue(temp);
+    return *this;
+}
+//postfix
+const PhysicalNumber& PhysicalNumber::operator++(int){
+    double temp = this->getValue();
+    PhysicalNumber returned(temp, this->getUnit());
+    temp++;
+    this->setValue(temp);
+    return returned;
+}
+//prefix
+const PhysicalNumber& PhysicalNumber::operator--(){
+    double temp = this->getValue();
+    temp--;
+    this->setValue(temp);
+    return *this;
+}
+//postfix
+const PhysicalNumber& PhysicalNumber::operator--(int){
+    double temp = this->getValue();
+    PhysicalNumber returned(temp, this->getUnit());
+    temp--;
+    this->setValue(temp);
+    return returned;
+}
+//continue here********#@!$#@%@$%$%
+const PhysicalNumber& PhysicalNumber::operator+=(const PhysicalNumber &other) const{
+    if(!areSameType(other)) throw std::invalid_argument("types not compatible!");
+    PhysicalNumber toReturn = this->convertNumber(other);
     PhysicalNumber temp(1,ariel::Unit::KM);
     return temp;
 }
-const PhysicalNumber PhysicalNumber::operator-() const{
+const PhysicalNumber& PhysicalNumber::operator-() const{
     PhysicalNumber temp(1,ariel::Unit::KM);
     return temp;
 }
 
-const PhysicalNumber PhysicalNumber::operator-=(const PhysicalNumber &other) const{
+const PhysicalNumber& PhysicalNumber::operator-=(const PhysicalNumber &other) const{
+    if(!areSameType(other)) throw std::invalid_argument("types not compatible!");
     PhysicalNumber temp(1,ariel::Unit::KM);
     return temp;
 }
-const PhysicalNumber PhysicalNumber::operator+(const PhysicalNumber &other) const{
+const PhysicalNumber& PhysicalNumber::operator+(const PhysicalNumber &other) const{
+    if(!areSameType(other)) throw std::invalid_argument("types not compatible!");
     PhysicalNumber temp(1,ariel::Unit::KM);
     return temp;
 }
-const PhysicalNumber PhysicalNumber::operator-(const PhysicalNumber &other) const{
+const PhysicalNumber& PhysicalNumber::operator-(const PhysicalNumber &other) const{
+    if(!areSameType(other)) throw std::invalid_argument("types not compatible!");
     PhysicalNumber temp(1,ariel::Unit::KM);
     return temp;
 }
 const bool PhysicalNumber::operator>(const PhysicalNumber& other)const{
+    if(!areSameType(other)) throw std::invalid_argument("types not compatible!");
     return false;
 }
 const bool PhysicalNumber::operator<(const PhysicalNumber& other)const{
+    if(!areSameType(other)) throw std::invalid_argument("types not compatible!");
     return false;
 }
 const bool PhysicalNumber::operator>=(const PhysicalNumber& other)const{
+    if(!areSameType(other)) throw std::invalid_argument("types not compatible!");
     return false;
 }
 const bool PhysicalNumber::operator<=(const PhysicalNumber& other)const{
+    if(!areSameType(other)) throw std::invalid_argument("types not compatible!");
     return false;
 }
 const bool PhysicalNumber::operator==(const PhysicalNumber& other)const{
+    if(!areSameType(other)) throw std::invalid_argument("types not compatible!");
     return false;
 }
 const bool PhysicalNumber::operator!=(const PhysicalNumber& other)const{
+    if(!areSameType(other)) throw std::invalid_argument("types not compatible!");
     return false;
 }
 std::ostream &ariel::operator<<(std::ostream &os, PhysicalNumber const &m){
@@ -57,4 +104,129 @@ std::ostream &ariel::operator<<(std::ostream &os, PhysicalNumber const &m){
 }
 std::istream &ariel::operator>>(std::istream &is, PhysicalNumber const &m){
     return is;
+}
+
+//private functions**************************************************************************************************
+
+bool PhysicalNumber::areSameType(const PhysicalNumber &other) const{
+    Unit thisUnit = this->getUnit();
+    Unit otherUnit = other.getUnit();
+    if(thisUnit == Unit::KM || thisUnit == Unit::M || thisUnit == Unit::CM){
+        return (otherUnit == Unit::KM || otherUnit == Unit::M || otherUnit == Unit::CM);
+    }
+    else if(thisUnit == Unit::HOUR || thisUnit == Unit::MIN || thisUnit == Unit::SEC){
+        return (otherUnit == Unit::HOUR || otherUnit == Unit::MIN || otherUnit == Unit::SEC);
+    }
+    else if(thisUnit == Unit::TON || thisUnit == Unit::KG || thisUnit == Unit::G){
+        return (otherUnit == Unit::TON || otherUnit == Unit::KM || otherUnit == Unit::M);
+    }
+    return false;
+}
+//assumes same type, i.e km and m
+bool PhysicalNumber::areSameScope(const PhysicalNumber &other) const{
+    Unit thisUnit = this->getUnit();
+    Unit otherUnit = other.getUnit();
+    return thisUnit == otherUnit;
+}
+
+//converts other to be the same scope as this
+const PhysicalNumber& PhysicalNumber::convertNumber(const PhysicalNumber &other) const{
+    Unit thisUnit = this->getUnit();
+    Unit otherUnit = other.getUnit();
+    int thisInt = (int)thisUnit;
+    int otherInt = (int)otherUnit;
+    int diff = thisInt - otherInt; //shows us which way sould we 'go' and how many steps should range from -2 to 2
+    double newValue;
+    if(this->areSameScope(other)) return other; //if there is no need to convert
+    if(thisUnit == Unit::KM || thisUnit == Unit::M || thisUnit == Unit::CM){
+        newValue = other.convertDistanceValue(diff);
+    }
+    else if(thisUnit == Unit::HOUR || thisUnit == Unit::MIN || thisUnit == Unit::SEC){
+        newValue = other.convertTimeValue(diff);
+    }
+    else if(thisUnit == Unit::TON || thisUnit == Unit::KG || thisUnit == Unit::G){
+        newValue = other.convertMassValue(diff);
+    }
+    PhysicalNumber toReturn(newValue, thisUnit);
+    return toReturn;
+}
+double PhysicalNumber::convertDistanceValue(int diff) const{
+    double newValue = this->getValue();
+    Unit unit = this->getUnit();
+    switch (diff)
+        {
+            case -2://this is cm and other is km
+                newValue = newValue*1000*100;
+                break;
+
+            case -1:
+                if(unit == Unit::KM){
+                    newValue = newValue*1000;
+                    break;
+                }
+                else{
+                    newValue = newValue*100;
+                    break;
+                }
+
+            case 1:
+                if(unit == Unit::M){
+                    newValue = newValue/100;
+                }
+                else newValue = newValue/1000;
+                break;
+
+            case 2:
+                newValue = newValue/100/1000;
+                break;
+        }
+        return newValue;
+}
+
+double PhysicalNumber::convertTimeValue(int diff) const{
+    double newValue = this->getValue();
+    Unit unit = this->getUnit();
+    switch (diff)
+        {
+            case -2://this is sec and other is hour
+                newValue = newValue*60*60;
+                break;
+
+            case -1:
+                newValue = newValue*60;
+                break;
+
+            case 1:
+                newValue = newValue/60;
+                break;
+
+            case 2:
+                newValue = newValue/60/60;
+                break;
+        }
+        return newValue;
+}
+
+double PhysicalNumber::convertTimeValue(int diff) const{
+    double newValue = this->getValue();
+    Unit unit = this->getUnit();
+    switch (diff)
+        {
+            case -2://this is g and other is ton
+                newValue = newValue*1000*1000;
+                break;
+
+            case -1:
+                newValue = newValue*1000;
+                break;
+
+            case 1:
+                newValue = newValue/1000;
+                break;
+
+            case 2:
+                newValue = newValue/1000/1000;
+                break;
+        }
+        return newValue;
 }
--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -1,22 +1,36 @@
 #include "Unit.h"
 #pragma once
 #include <iostream>
+#include<string>
 namespace ariel{
     class PhysicalNumber{
         private:
         double value;
         ariel::Unit unit;
+        bool areSameType(const PhysicalNumber& other) const;
+        bool areSameScope(const PhysicalNumber &other) const; //km and m, tom and kg, not kg and km
+        const PhysicalNumber& convertNumber(const PhysicalNumber &other) const;
+        double convertDistanceValue(int diff) const;
+        double convertTimeValue(int diff) const;
+        double convertMassValue(int diff) const;
         public:
+        //std::string units[9] = {"KM","M","CM","HOUR","MIN","SEC","TON", "KG", "G"};
         PhysicalNumber(double num, Unit unit);
+        //getters and setters
+        double getValue() const;
+        Unit getUnit() const;
+        void setValue(double x);
+        void setUnit(Unit u);
         //arithmetics
-        const PhysicalNumber operator+(const PhysicalNumber& other) const;
-        const PhysicalNumber operator+=(const PhysicalNumber& other) const;
-        const PhysicalNumber operator-(const PhysicalNumber& other) const;
-        const PhysicalNumber operator-=(const PhysicalNumber& other) const;
-        const PhysicalNumber operator-() const;
-        const PhysicalNumber operator--();
-        const PhysicalNumber operator++();
-
+        const PhysicalNumber& operator+(const PhysicalNumber& other) const;
+        const PhysicalNumber& operator+=(const PhysicalNumber& other) const;
+        const PhysicalNumber& operator-(const PhysicalNumber& other) const;
+        const PhysicalNumber& operator-=(const PhysicalNumber& other) const;
+        const PhysicalNumber& operator-() const;
+        const PhysicalNumber& operator--();
+        const PhysicalNumber& operator--(int);
+        const PhysicalNumber& operator++();
+        const PhysicalNumber& operator++(int);
         //compare
         const bool operator>(const PhysicalNumber& other)const;
         const bool operator<(const PhysicalNumber& other)const;
